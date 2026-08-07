cfe4cef990c0247ba954ef18bfca9e72ffab59e6
Shubham Mahato <86435647+shubham270706@users.noreply.github.com>
2026-08-07 18:18:30 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# AutoBDP - Automated Binary Decompiler Pipeline
+# Copyright (C) 2026 Shubham Mahato (oopsiedoopsie)
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+
 # Enforce root privileges
 if [[ $EUID -ne 0 ]]; then
    echo "[-] Error: install.sh must be run with root privileges (sudo)."
