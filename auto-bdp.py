499b5cef6a5e5fd2fe648c43f852c5ef2c78307b
Shubham Mahato <86435647+shubham270706@users.noreply.github.com>
2026-08-07 18:18:04 +0530

    Update auto-bdp.py

--- auto-bdp.py
+++ auto-bdp.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python3
-#@author: Shubham Mahato (oopsiedoopsie)
+# AutoBDP - Automated Binary Decompiler Pipeline
+# Copyright (C) 2026 Shubham Mahato (oopsiedoopsie)
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+
 #@github: https://github.com/shubham270706
 #@LinkedIN: https://www.linkedin.com/in/shubham-mahato-0ba387299/
 #@Discord: oopsie_doopsie224
+
+
 
 import subprocess
 import os
